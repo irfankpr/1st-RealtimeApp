61006aaf1aef701308069e94a4732e2635de26c3
irfankpr <irfanmuhammedkpr@gmail.com>
2023-06-22 15:21:57 +0530

    1st - but not working

---
+++ randnum/asgi.py
@@ -0,0 +1,13 @@
+import os
+from channels.routing import ProtocolTypeRouter
+from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+from channels.routing import URLRouter
+from main.routing import ws_urlpatterns
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'randnum.settings')
+
+application = ProtocolTypeRouter({
+    'http': get_asgi_application(),
+    'websocket': AuthMiddlewareStack(URLRouter(ws_urlpatterns))
+})
